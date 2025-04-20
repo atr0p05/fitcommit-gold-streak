32ab2b0f4a8d4e718d424d82aa5aaf7a70e7ac5c
atr0p05 <34943820+atr0p05@users.noreply.github.com>
2025-04-19 22:03:23 -0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.665782f6396241538df29ab0a2549747',
-  appName: 'fitcommit-gold-streak',
+  appId: 'com.fitcommit.goldstreak',
+  appName: 'FitCommit',
   webDir: 'dist',
   server: {
     url: 'https://665782f6-3962-4153-8df2-9ab0a2549747.lovableproject.com?forceHideBadge=true',
