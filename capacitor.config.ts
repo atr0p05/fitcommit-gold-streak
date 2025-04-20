382dee844d69da5820baefbb80e1607b7f1b603a
atr0p05 <34943820+atr0p05@users.noreply.github.com>
2025-04-19 22:04:11 -0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.fitcommit.goldstreak',
+  appId: 'com.fitcommit.app',
   appName: 'FitCommit',
   webDir: 'dist',
   server: {
